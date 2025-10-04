fed70ed2659be54892a2809e108cf3faf1af4b0e
Fanoussee <stephanie.t16@laposte.net>
2025-10-04 12:45:37 +0200

    updating router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './css/index.css';
+import Home from './components/Home';
+import PlayersNumber from './components/PlayersNumber';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter basename='/dyscogrid'>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/nombre-joueurs" element={<PlayersNumber />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
