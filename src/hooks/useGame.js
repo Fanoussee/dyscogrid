e7f5ba46ef86e2e798d2714bcd25e555d9d61303
Fanoussee <stephanie.t16@laposte.net>
2025-10-06 09:35:20 +0200

    adding possiblePlayersNumber for PlayersNumber page

--- src/hooks/useGame.js
+++ src/hooks/useGame.js
@@ -1,11 +1,14 @@
 import { useState } from "react";
-
+import { range } from 'ramda'
 const useGame = () => {
   const [playersNumber, setPlayersNumber] = useState(0);
+  const possiblePlayersNumber = range(1, 5);
+
   return {
     gameHandler: {
       playersNumber,
-      setPlayersNumber
+      setPlayersNumber,
+      possiblePlayersNumber
     }
   }
 }
--- src/pages/PlayersNumber.jsx
+++ src/pages/PlayersNumber.jsx
@@ -1,17 +1,20 @@
+import { map } from "ramda";
 import Title from "../components/Title"
-import Button from "../formComponents/Button"
+import ButtonLink from "../formComponents/ButtonLink"
 
 const PlayersNumber = ({ gameHandler }) => {
-  const { setPlayersNumber } = gameHandler;
+  const { setPlayersNumber, possiblePlayersNumber } = gameHandler;
 
   return (
     <>
       <Title title={"Combien y-a-t-il de joueurs ?"} />
       <div className="group-buttons">
-        <Button label={"1 joueur"} onClick={() => setPlayersNumber(1)} />
-        <Button label={"2 joueurs"} onClick={() => setPlayersNumber(2)} />
-        <Button label={"3 joueurs"} onClick={() => setPlayersNumber(3)} />
-        <Button label={"4 joueurs"} onClick={() => setPlayersNumber(4)} />
+        { map(
+          playerNumber => <ButtonLink 
+            label={`${playerNumber} joueur${playerNumber > 1 ? "s" : ""}`} 
+            onClick={() => setPlayersNumber(playerNumber)}
+          />
+        )(possiblePlayersNumber) }
       </div>
     </>
   )
