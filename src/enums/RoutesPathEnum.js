e9a07b054b558a22e0ca221c2e0b75d732d970c6
Fanoussee <stephanie.t16@laposte.net>
2025-10-06 12:01:20 +0200

    adding route path

--- src/enums/RoutesPathEnum.js
+++ src/enums/RoutesPathEnum.js
@@ -1,7 +1,8 @@
 const RoutesPathEnum = Object.freeze({
   origin: "/",
   playersNumber: "/nombre-joueurs",
-  playersFirstNames: "/prenoms-joueurs"
+  playersFirstNames: "/prenoms-joueurs",
+  gameTurn: "/tour-de-jeu"
 });
 
 export default RoutesPathEnum;
